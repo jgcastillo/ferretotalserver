cfd6f3ef00ef0c4224c10c28a2a71ea745622b5a
jgcastillo <jgcastillo@SPONTE08-PC.SPONTECORP.INTERNAL>
2013-10-11 14:56:41 -0430

    Carga inicial en jgcastillo

--- src/java/com/spontecorp/ferretotalserver/service/UsuarioService.java
+++ src/java/com/spontecorp/ferretotalserver/service/UsuarioService.java
@@ -12,18 +12,22 @@ import javax.persistence.Query;
  *
  * @author jgcastillo
  */
-public class UsuarioService extends UsuarioFacade{
- 
+public class UsuarioService extends UsuarioFacade {
+
     public Usuario findUsuario(String user) {
         //EntityManager em = getEntityManager();
         EntityManagerFactory emf = Persistence.createEntityManagerFactory(AbstractFacade.PERSISTENCE_UNIT);
         EntityManager em = emf.createEntityManager();
+        Usuario usuario = null;
         try {
             Query query = em.createNamedQuery("Usuario.findByUser", Usuario.class);
             query.setParameter("user", user);
-            return (Usuario) query.getSingleResult();
+            usuario = (Usuario) query.getSingleResult();
+        }catch (Exception e){
+            System.out.println("Está dando el siguiente error: " + e.getMessage());
         } finally {
             em.close();
+            return usuario;
         }
     }
 }
